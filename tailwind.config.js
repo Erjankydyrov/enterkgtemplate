9f9a03617deb27620803fdf68f09399fb7104ff4
Erjankydyrov <erjankydyrov29@gmail.com>
2022-11-15 16:08:15 +0600

    corrected container width and prefixs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "900px",
+      xl: "1100px",
+    },
   },
   plugins: [],
-}
+};
